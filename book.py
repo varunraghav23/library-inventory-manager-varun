071540b73bec69e9d16ae3b1a56bbf40e16e69b5
varunraghav23 <varunkumarraghav12345@gmail.com>
2025-11-23 19:05:00 +0530

    Update book.py

--- book.py
+++ book.py
@@ -1,1 +1,9 @@
+class Book:
+    def __init__(self, title, author, isbn, status="available"):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.status = status
 
+    def __str__(self):
+        return f"{self.title} by {self.author} | ISBN: {self.isbn} | Status: {self.status}"
